67c1bc958d8651480ce6853edd319f347749294c
viga681101 <viga681101@hotmail.com>
2023-10-27 12:21:35 -0700

    Patron de repositorio

--- SistemaInventariosV7/Program.cs
+++ SistemaInventariosV7/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SistemaInventariosV7.AccesoDatos.Data;
+using SistemaInventariosV7.AccesoDatos.Repositorio;
+using SistemaInventariosV7.AccesoDatos.Repositorio.IRepositorio;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddScoped<IUnidadTrabajo, UnidadTrabajo>();
+
 
 var app = builder.Build();
 
