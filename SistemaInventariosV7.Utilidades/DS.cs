6accaf9a8d370f20e0389c4580e6ae5eec251efd
viga681101 <viga681101@hotmail.com>
2023-11-02 11:44:18 -0700

    Registro de Usuarios

--- SistemaInventariosV7.Utilidades/DS.cs
+++ SistemaInventariosV7.Utilidades/DS.cs
@@ -11,7 +11,9 @@ namespace SistemaInventariosV7.Utilidades
         public const string Exitosa = "Exitosa";
         public const string Error = "Error";
         public const string ImagenRuta = @"\imagenes\producto\";
-
+        public const string Role_Admin = "Admin";
+        public const string Role_Cliente = "Cliente";
+        public const string Role_Inventario = "Inventario";
 
     }
 }
